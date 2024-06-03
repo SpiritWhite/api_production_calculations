68ffb789f8c31085f2d6bd7238e6d183f2614c49
jcperez <jcperez@lafise.com>
2024-06-03 09:22:32 -0600

    Fixed entities

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,7 +9,6 @@ export class AuthController {
   @HttpCode(200)
   @Post('sign-in')
   signIn(@Body() singInAuthDto: SingInAuthDto) {
-    console.log(singInAuthDto);
     return this.authService.signIn(singInAuthDto);
   }
 
